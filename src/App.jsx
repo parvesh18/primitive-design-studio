6290dfa560d7218bf6d3b5b5712cc6b82a9cbc09
Parvesh Yadav <montyyadav123789@gmail.com>
2024-11-21 16:50:51 +0530

    Add routing to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import AppLayout from "./AppLayout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import WorkPage from "./components/WorkPage";
+import ClientPage from "./components/ClientPage";
+import ContactPage from "./components/ContactPage";
 
 const App = () => {
-  return <AppLayout />;
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/about",
+          element: <AboutPage />,
+        },
+        {
+          path: "/work",
+          element: <WorkPage />,
+        },
+        {
+          path: "/client",
+          element: <ClientPage />,
+        },
+        {
+          path: "/contact",
+          element: <ContactPage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={appRouter} />;
 };
 
 export default App;
